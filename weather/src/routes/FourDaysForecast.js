6be1d19cc13ee45265d21ff20bdba51a2a230a0c
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-04-06 22:50:54 +0800

    update 4 days weather
    display icon beside the weather forecast
    added 5 color icons to assets/icons

--- weather/src/routes/FourDaysForecast.js
+++ weather/src/routes/FourDaysForecast.js
@@ -1,34 +1,46 @@
 
 export default function FourDaysForecast(props){
+    let newArray=props.fourdayData.forecasts
     return(
         <>  
             <div>
-                <h2 className="container_headerbox" style={{textAlign:"center"}}>Forecasts for Next 4 days</h2>
+                <h2 className="container_headerbox">Forecasts for Next 4 days</h2>
             </div>
                 {props.fourdayData.forecasts===undefined ? null 
                 : 
                 <table>
                   <tbody>
-                    <tr>
-                        <td>Weather Icon</td>
-                        <td>{props.fourdayData.forecasts[0].forecast}</td>
-                    </tr>
-                    <tr>
-                        <td>Weather Icon</td>
-                        <td>{props.fourdayData.forecasts[1].forecast}</td>
-                    </tr>
-                    <tr>
-                        <td>Weather Icon</td>
-                        <td>{props.fourdayData.forecasts[2].forecast}</td>
-                    </tr>
-                    <tr>
-                        <td>Weather Icon</td>
-                        <td>{props.fourdayData.forecasts[3].forecast}</td>
-                    </tr>
+                    {newArray.map((forecasts,index)=>(
+                        <tr key={index}>
+                          <td>{weatherIcon(forecasts.forecast)}</td>
+                          <td>{forecasts.forecast}</td>
+                        </tr>
+                    ))}    
                   </tbody>
                 </table>
                 }
-    
         </>
     )
+}
+
+function weatherIcon(forecast){
+    
+    if (forecast.includes("thundery showers")) {
+        return <img className="fourDayIcon" src={require("../assets/icons/thundery-showers.png")} alt="rainy"/>
+    }
+    if (forecast.includes("partly cloudy")) {
+        return <img className="fourDayIcon" src={require("../assets/icons/partly-cloudy.png")} alt="cloudy"/>
+    }
+    if (forecast.includes("cloudy")) {
+        return <img className="fourDayIcon" src={require("../assets/icons/cloudy.png")} alt="cloudy"/>
+    }
+    if (forecast.includes("sunny")) {
+        return <img className="fourDayIcon" src={require("../assets/icons/sunny.png")} alt="sunny"/>
+    }
+    if (forecast.includes("light rain")) {
+        return <img className="fourDayIcon" src={require("../assets/icons/light-rain.png")} alt="rainy"/>
+    }
+    
+   return <img className="fourDayIcon" src={require("../assets/icons/happy.png")} alt="happy"/>
+
 }
